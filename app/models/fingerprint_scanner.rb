f1e54deffd02d58477d3010062885aa31e963ba1
Kphillycat <keithwilliamsjr@gmail.com>
2014-01-11 17:35:57 -0500

    Add code in finger_scanner.rb for tests in fingerprint_scanner_spec.rb

--- app/models/fingerprint_scanner.rb
+++ app/models/fingerprint_scanner.rb
@@ -3,4 +3,8 @@ class FingerprintScanner < ActiveRecord::Base
 
   has_many :scanned_fingerprints
   has_many :fingerprints, through: :scanned_fingerprints
+
+  def self.has_scanned_fingerprints_for_database_id(db_id)
+  	FingerprintScanner.where(id: ScannedFingerprint.select(:fingerprint_scanner_id).where(fingerprint_id: Fingerprint.select(:id).where(fingerprint_database_id: db_id)))
+  end
 end
