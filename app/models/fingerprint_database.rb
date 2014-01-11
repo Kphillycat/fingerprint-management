01280529b72bb0dc39a92200f2a728ac065d12fb
Kphillycat <keithwilliamsjr@gmail.com>
2014-01-11 17:58:41 -0500

    Add code in fingerprint_database.rb for tests in fingerprint_database_spec.rb

--- app/models/fingerprint_database.rb
+++ app/models/fingerprint_database.rb
@@ -2,4 +2,12 @@ class FingerprintDatabase < ActiveRecord::Base
   attr_accessible :name, :owner
 
   has_many :fingerprints
+
+  def unknown_people_fingerpints
+  	Fingerprint.where(fingerprint_database_id: self.id, person_id: nil)
+  end
+
+  def self.fingerprint_counts
+  	Fingerprint.group(:fingerprint_database_id).count
+  end
 end
